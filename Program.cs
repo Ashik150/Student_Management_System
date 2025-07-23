428b489cfe15d2131fd22caa86d4ac8bcb539efc
Ashik150 <sskhan4409@gmail.com>
2025-07-23 22:02:05 +0600

    Main class implement

--- Program.cs
+++ Program.cs
@@ -56,6 +56,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        System.Console.WriteLine("Hello, World!");
+        try
+        {
+            Student s1 = new Student("Ashik Khan", new DateTime(2001, 05, 30), "210042150");
+            Student s2 = new Student("Shahriar Khan", new DateTime(2002, 05, 30), "210042153");
+
+            System.Console.WriteLine($"Name: {s1.Name}, Date of Birth: {s1.DateOfBirth.ToShortDateString()}, Student ID: {s1.StudentId}"
+                + $"\nName: {s2.Name}, Date of Birth: {s2.DateOfBirth.ToShortDateString()}, Student ID: {s2.StudentId}");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error: {ex.Message}");
+        }
     }
 }
--- obj/Debug/net9.0/Student_Management_System.AssemblyInfo.cs
+++ obj/Debug/net9.0/Student_Management_System.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Student_Management_System")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bbff7a69f74aaadff4a80395b4ac68c327afd92d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f849400f5542f22fb09ae2c9f92b4de99d61020")]
 [assembly: System.Reflection.AssemblyProductAttribute("Student_Management_System")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Student_Management_System")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
